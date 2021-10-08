b553d78bb5225ed7f0413574952e01afd0dffcc0
Insthync <ittipon.bay@gmail.com>
2021-10-08 09:49:07 +0700

    Add TextureQualitySetting

---
+++ Scripts/TextureQualitySetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GraphicSettings
+{
+    public class TextureQualitySetting : MonoBehaviour
+    {
+        public int value = 0;
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            QualitySettings.masterTextureLimit = 0;
+        }
+    }
+}
