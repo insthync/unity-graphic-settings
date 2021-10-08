268f3d6e0b55c0e29c5d904bcb7083e9343b8070
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:00:01 +0700

    Add shadows setting

---
+++ Scripts/ShadowsSetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GraphicSettings
+{
+    public class ShadowsSetting : MonoBehaviour
+    {
+        public ShadowQuality setting = ShadowQuality.Disable;
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            QualitySettings.shadows = setting;
+        }
+    }
+}
