bceab41a95de89a2ff9a651330f01659069ab2e3
Insthync <ittipon.bay@gmail.com>
2021-10-08 09:59:52 +0700

    Add anti aliasing setting

---
+++ Scripts/AntiAliasingSetting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GraphicSettings
+{
+    public class AntiAliasingSetting : MonoBehaviour
+    {
+        public enum Setting : int
+        {
+            Disabled = 0,
+            x2 = 2,
+            x4 = 4,
+            x8 = 8,
+        }
+        public Setting setting = Setting.Disabled;
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            QualitySettings.antiAliasing = (int)setting;
+        }
+    }
+}
