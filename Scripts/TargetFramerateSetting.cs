0a232ed9aae81d151dc840571e32fa2cb3fa50be
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:02:16 +0700

    Add target framerate setting

---
+++ Scripts/TargetFramerateSetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GraphicSettings
+{
+    public class TargetFramerateSetting : MonoBehaviour
+    {
+        public int setting = 60;
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            Application.targetFrameRate = setting;
+        }
+    }
+}
