d7f0f726068bca04e04f83e866174df6575046a9
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:10:28 +0700

    Add anisotropic filtering setting

---
+++ Scripts/AnisotropicFilteringSetting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GraphicSettings
+{
+    public class AnisotropicFilteringSetting : MonoBehaviour
+    {
+        public AnisotropicFiltering setting = AnisotropicFiltering.Disable;
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            QualitySettings.anisotropicFiltering = setting;
+        }
+    }
+}
