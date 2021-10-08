9b56ea106c4c3fc6c450a850dbfb6053180fc343
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:16:45 +0700

    Add resolution scaling setting

---
+++ Scripts/ResolutionScalingSetting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicSettings
+{
+    public class ResolutionScalingSetting : MonoBehaviour
+    {
+        public Slider slider;
+
+        private void Start()
+        {
+            slider.minValue = 0.1f;
+            slider.maxValue = 1f;
+            slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            QualitySettings.resolutionScalingFixedDPIFactor = value;
+        }
+    }
+}
