b16e4453bd25f23aaab20a82f779d3816919c843
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:37:38 +0700

    Add full screen setting

---
+++ Scripts/FullScreenSetting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicSettings
+{
+    public class FullScreenSetting : MonoBehaviour
+    {
+        public FullScreenMode setting = FullScreenMode.ExclusiveFullScreen;
+        public Toggle toggle;
+        public Button button;
+
+        private void Start()
+        {
+            if (toggle != null)
+            {
+                toggle.SetIsOnWithoutNotify(Screen.fullScreenMode == setting);
+                toggle.onValueChanged.AddListener(OnToggle);
+            }
+            if (button != null)
+                button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDestroy()
+        {
+            if (toggle != null)
+                toggle.onValueChanged.RemoveListener(OnToggle);
+            if (button != null)
+                button.onClick.RemoveListener(OnClick);
+        }
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            Screen.fullScreenMode = setting;
+        }
+    }
+}
