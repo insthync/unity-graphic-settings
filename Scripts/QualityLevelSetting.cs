abbfb6394d0c82934f70f4909f0b47beda2736c8
Insthync <ittipon.bay@gmail.com>
2021-10-08 10:21:50 +0700

    Add quality level setting

---
+++ Scripts/QualityLevelSetting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicSettings
+{
+    public class QualityLevelSetting : MonoBehaviour
+    {
+        public Toggle toggle;
+        public int setting;
+
+        private void Start()
+        {
+            if (toggle != null)
+                toggle.SetIsOnWithoutNotify(QualitySettings.GetQualityLevel() == setting);
+        }
+
+        public void OnToggle(bool isOn)
+        {
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            QualitySettings.SetQualityLevel(setting);
+        }
+    }
+}
