75445f694b3e33d09f4b92bef2a877b7103dc24c
Insthync <ittipon.bay@gmail.com>
2021-10-10 03:56:24 +0700

    Add shadow distance setting

---
+++ Scripts/ShadowDistanceSetting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicSettings
+{
+    public class ShadowDistanceSetting : MonoBehaviour, IGraphicSetting
+    {
+        public const string SAVE_KEY = "GRAPHIC_SETTING_SHADOW_DISTANCE";
+        public Slider slider;
+        public bool applyImmediately = true;
+        public bool ApplyImmediately { get { return applyImmediately; } set { applyImmediately = value; } }
+
+        private void Start()
+        {
+            slider.minValue = 0.1f;
+            slider.maxValue = 1f;
+            slider.SetValueWithoutNotify(QualitySettings.shadowDistance);
+            slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnDestroy()
+        {
+            slider.onValueChanged.RemoveListener(OnValueChanged);
+        }
+
+        public void OnValueChanged(float value)
+        {
+            if (ApplyImmediately)
+                Apply();
+        }
+
+        public void Apply()
+        {
+            QualitySettings.shadowDistance = slider.value;
+            PlayerPrefs.SetFloat(SAVE_KEY, slider.value);
+            PlayerPrefs.Save();
+        }
+
+        public static void Load()
+        {
+            if (PlayerPrefs.HasKey(SAVE_KEY))
+                QualitySettings.shadowDistance = PlayerPrefs.GetFloat(SAVE_KEY);
+        }
+    }
+}
