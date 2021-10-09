ab5d10b41131ee3e1aff39bddc6587bb84aff46f
Insthync <ittipon.bay@gmail.com>
2021-10-10 03:56:11 +0700

    Add realtime reflection probes setting

---
+++ Scripts/RealtimeReflectionProbesSetting.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GraphicSettings
+{
+    public class RealtimeReflectionProbesSetting : MonoBehaviour, IGraphicSetting
+    {
+        public const string SAVE_KEY = "GRAPHIC_SETTING_REALTIME_REFLECTION_PROBES";
+        public bool setting;
+        public Toggle toggle;
+        public Button button;
+        public bool applyImmediately = true;
+        public bool ApplyImmediately { get { return applyImmediately; } set { applyImmediately = value; } }
+
+        private bool isOn;
+
+        private void Start()
+        {
+            if (toggle != null)
+            {
+                toggle.SetIsOnWithoutNotify(QualitySettings.realtimeReflectionProbes == setting);
+                toggle.onValueChanged.AddListener(OnToggle);
+            }
+            if (button != null)
+                button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDestroy()
+        {
+            if (toggle != null)
+                toggle.onValueChanged.RemoveListener(OnToggle);
+            if (button != null)
+                button.onClick.RemoveListener(OnClick);
+        }
+
+        public void OnToggle(bool isOn)
+        {
+            this.isOn = isOn;
+            if (isOn)
+                OnClick();
+        }
+
+        public void OnClick()
+        {
+            if (ApplyImmediately)
+                Apply();
+        }
+
+        public void Apply()
+        {
+            if (isOn)
+            {
+                QualitySettings.realtimeReflectionProbes = setting;
+                PlayerPrefs.SetInt(SAVE_KEY, setting ? 1 : 0);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public static void Load()
+        {
+            if (PlayerPrefs.HasKey(SAVE_KEY))
+                QualitySettings.realtimeReflectionProbes = PlayerPrefs.GetInt(SAVE_KEY) > 0;
+        }
+    }
+}
