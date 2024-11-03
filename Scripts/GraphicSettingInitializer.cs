4147eb675a4b2f7d2c8f52a52c51c951e5ee6e5f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-11-03 21:09:01 +0700

    feat: avoid null data error

--- Scripts/GraphicSettingInitializer.cs
+++ Scripts/GraphicSettingInitializer.cs
@@ -5,24 +5,167 @@
         [UnityEngine.RuntimeInitializeOnLoadMethod(UnityEngine.RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Initialize()
         {
-            QualityLevelSetting.Load();
-            FullScreenSetting.Load();
-            ScreenResolutionSetting.Load();
-            AnisotropicFilteringSetting.Load();
-            AntiAliasingSetting.Load();
-            TextureQualitySetting.Load();
-            ShadowsSetting.Load();
-            ShadowDistanceSetting.Load();
-            SoftParticlesSetting.Load();
-            RealtimeReflectionProbesSetting.Load();
-            TargetFramerateSetting.Load();
-            ResolutionScalingSetting.Load();
-            UrpAntiAliasingSetting.Load();
-            UrpShadowResolutionSetting.Load();
-            UrpShadowDistanceSetting.Load();
-            UrpShadowCascadeSetting.Load();
-            UrpSoftShadowSetting.Load();
-            UrpResolutionScalingSetting.Load();
+            try
+            {
+                QualityLevelSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                FullScreenSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                ScreenResolutionSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                AnisotropicFilteringSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                AntiAliasingSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                TextureQualitySetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                ShadowsSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                ShadowDistanceSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                SoftParticlesSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                RealtimeReflectionProbesSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                TargetFramerateSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                ResolutionScalingSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpAntiAliasingSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpShadowResolutionSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpShadowDistanceSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpShadowCascadeSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpSoftShadowSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+
+            try
+            {
+                UrpResolutionScalingSetting.Load();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
         }
     }
 }
